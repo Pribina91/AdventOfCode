65ae787ebc56b4ebb61a2a74fbe53bfc23e303a6
Andrej Stajer <astajer@pribitech.com>
2020-12-03 22:41:20 +0700

    3 finished

---
+++ AdventOfCode.3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = File.ReadAllLines("input.txt");
+           // var items = File.ReadAllLines("sample.txt");
+
+            var speeds = new List<(int down, int right)>()
+            {
+                (1,1),
+                (1,3),
+                (1,5),
+                (1,7),
+                (2,1),
+            };
+            long result = 1; 
+            foreach (var speed in speeds)
+            {
+                var treesCount = GetTreesDownHill(speed, items).Count();
+                Console.WriteLine($"down-{speed.down} right-{speed.right} count- {treesCount}");
+                result *= treesCount;
+            }
+
+           
+           // var count = treesCount.Count();
+            Console.WriteLine($"total- {result}");
+        }
+
+        private static IEnumerable<(int x, int y)> GetTreesDownHill((int down,int right) speed, string[] items)
+        {
+            var yPosition = 0;
+            for (int i = 0; i < items.Length; i += speed.down)
+            {
+                var line = items[i];
+               
+                if (line[yPosition] == '#')
+                {
+                    yield return (i, yPosition);
+                }
+
+                yPosition += speed.right;
+                if (yPosition >= line.Length)
+                {
+                    yPosition -= line.Length;
+                }
+            }
+
+        }
+    }
+}
