3635e0836754afb12fe7b71abf0617973ab83876
Andrej Stajer <astajer@pribitech.com>
2020-12-03 21:21:44 +0700

    git ignore and sln file

---
+++ AdventOfCode.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
